0f722541140e453eaefa2f10f6b759d2df2b2269
Kun Woo Kim <kimkuns98@gmail.com>
2024-02-18 11:10:18 +0900

    로그인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import Hero from '@/components/main/Hero';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
   const { data: session } = useSession();
 
-  if (!session) return '/login';
+  if (!session) return router.push('/login');
 
   return (
     <div className="container flex h-screen w-full flex-col gap-4 py-4">
